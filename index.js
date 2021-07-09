d5d88acb2ff0af9415b5223beeeac204b0608282
Santiago M <smonsalvemej2019@fau.edu>
2021-07-09 10:03:20 -0400

    index

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 const axios = require('axios')
 
-console.log(axios)
+var config = {
+    method: 'get',
+    url: 'https://santiago-test.atlassian.net/wiki/rest/api/content/393218/child/attachment',
+    headers: { 
+        'Accept': 'application/json', 
+        'Authorization': 'Basic c21vbnNhbHZlbWVqMjAxOUBmYXUuZWR1OkJNZnJHSm5UNTFMRXRGVVdxWFpaOTRCNg=='
+    }
+};
+
+axios(config)
+.then(function (response) {
+console.log(JSON.stringify(response.data));
+})
+.catch(function (error) {
+console.log(error);
+});
